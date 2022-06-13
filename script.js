0c4e9b45c03c5680c25ce8a1f4cff12d6402e8b9
Diogo <101531996+wukezin@users.noreply.github.com>
2022-06-13 15:20:37 -0300

    enhancements
    enhancements in interactions

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const input = document.querySelectorAll('.input');
 const button = document.querySelector('.loginButton')
 
+
+
 const handleFocus = ({target}) => {
   const span  = target.previousElementSibling;
   span.classList.add('spanOn')
@@ -32,3 +34,45 @@ input.forEach((input) => {
 input.forEach((input) => {
   input.addEventListener('input', handleChange)
 });
+
+
+
+function colorButtonOver() {
+  button.style.backgroundColor= '#B40404'
+}
+
+function colorButtonOut() {
+  button.style.backgroundColor= '#CF3C3F'
+}
+
+function linksOver(links) {
+  links.style.color='#151515'
+}
+
+function linksOut(links) {
+  links.style.color='#A7A7A7'
+}
+
+function facebookOver(face){
+  face.style.backgroundColor='#0f5fc7'
+}
+function facebookOut(face){
+  face.style.backgroundColor='#1b77f3'
+}
+
+function googleOver(google){
+  google.style.backgroundColor='#e2dfdf'
+}
+
+function googleOut(google){
+  google.style.backgroundColor='#ECECEC'
+}
+
+function appleOver(apple){
+  apple.style.backgroundColor='#1d1d1d'
+}
+
+function appleOut(apple){
+  apple.style.backgroundColor='#000000'
+}
+
